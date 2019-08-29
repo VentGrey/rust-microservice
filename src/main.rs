a64d53b6389b75d44a9db328aa39a5dbcca50f91
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-08-28 22:47:02 -0500

    Rebuild dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,26 @@
+extern crate hyper;
+extern crate futures;
+
 #[macro_use]
 extern crate log;
 extern crate env_logger;
 
-use hyper::server::{Request, Response, Service};
+use hyper::Request;
+use hyper::Response;
+use hyper::service::Service;
+
 use futures::future::Future;
 
 struct Microservice;
 
 impl Service for Microservice {
-    type Request = Request;
-    type Response = Response;
+    type ReqBody = Self::ReqBody;    //type Request = Request;
+    type ResBody = Self::ResBody;    //type Response = Response;
     type Error = hyper::Error;
-    type Future = Box<dyn Future<Item = Self::Response, Error = Self::Error>>;
+    type Future = Box<dyn Future<Item = Self::ResBody, Error = Self::Error>>;
 
-    fn call(&self, request: Request) -> Self::Future {
-        info!("El microservicio recibió una petición: {:?}", request);
+    fn call(&self, request: Self::ReqBody) -> Self::Future {
+        info!("Microservice received a request: {:?}", request);
         Box::new(futures::future::ok(Response::new()))
     }
 }
