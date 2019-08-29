45f780f4461ce2ac43e34277b194efe4d940ce5b
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-08-28 22:48:02 -0500

    Now it works. Sorry for old deps

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,20 @@
-extern crate hyper;
-extern crate futures;
-
 #[macro_use]
 extern crate log;
 extern crate env_logger;
 
-use hyper::Request;
-use hyper::Response;
-use hyper::service::Service;
+use hyper::server::{Request, Response, Service};
 
 use futures::future::Future;
 
 struct Microservice;
 
 impl Service for Microservice {
-    type ReqBody = Self::ReqBody;    //type Request = Request;
-    type ResBody = Self::ResBody;    //type Response = Response;
+    type Request = Request;
+    type Response = Response;
     type Error = hyper::Error;
-    type Future = Box<dyn Future<Item = Self::ResBody, Error = Self::Error>>;
+    type Future = Box<dyn Future<Item = Self::Response, Error = Self::Error>>;
 
-    fn call(&self, request: Self::ReqBody) -> Self::Future {
+    fn call(&self, request: Request) -> Self::Future {
         info!("Microservice received a request: {:?}", request);
         Box::new(futures::future::ok(Response::new()))
     }
